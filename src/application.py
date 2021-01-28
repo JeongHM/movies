db229df08c7ddc1c231ea3a400fc115fde7e1452
JeongHM <kjhm0607@gmail.com>
2021-01-28 17:03:26 +0900

    [feat] application blueprint, cors, model init

--- src/application.py
+++ src/application.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from flask_cors import CORS
+
+from src.api.v1.models.movies import MoviesModel
+from src.api.common.constants import CONSTANTS
 
 
 def create_app():
+    """
+    Setting app config, logging, cors ..
+    :return: Flask()
+    """
+    # Set Flask App
     app = Flask(import_name=__name__)
+
+    # Set Blueprint
+    from src.api.v1.controllers.movies import movies_blueprint
+    app.register_blueprint(blueprint=movies_blueprint,
+                           url_prefix=CONSTANTS["V1_MOVIE_API"])
+
+    # Set CORS
+    CORS(app=app)
+
+    # Create table
+    MoviesModel().init_table()
 
     return app
 

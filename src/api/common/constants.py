9176c61edf9c29235cb70e765fc1e51ed952894f
JeongHM <kjhm0607@gmail.com>
2021-02-01 14:53:10 +0900

    [env] constant api route

--- src/api/common/constants.py
+++ src/api/common/constants.py
@@ -2,5 +2,6 @@ from os import path
 
 CONSTANTS = {
     "DATABASE_PATH": path.abspath(path.join(path.dirname(__file__),
-                                            "../../ticket.db"))
+                                            "../../ticket.db")),
+    "V1_MOVIE_API": "/api/v1/movies"
 }
